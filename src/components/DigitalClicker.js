17b96d77f8653291c341093ab3877ce3da69da2d
Dice-K39 <daisukehashimoto39@gmail.com>
2020-12-31 16:21:49 -0500

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,31 @@
 // Code DigitalClicker Component Here
+import React, { Component } from 'react';
+
+class DigitalClicker extends Component
+{
+    constructor()
+    {
+        super();
+        this.state =
+        {
+            timesClicked: 0
+        };
+    }
+
+    handleOnClick = () =>
+    {
+        this.setState(previousState => 
+        {
+            return {
+                timesClicked: previousState.timesClicked + 1
+            }; 
+        });
+    }
+
+    render()
+    {
+        return <button onClick={this.handleOnClick}>{this.state.timesClicked}</button>
+    }
+}
+
+export default DigitalClicker;
